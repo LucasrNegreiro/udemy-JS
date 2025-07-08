bf182927f71e17b43d7349b004cae3c3339cde1f
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 11:08:58 -0300

    atualização
    atualizando o codigo e aprendendo os epescials numbers

---
+++ numbers/1-numbers/script.js
@@ -0,0 +1,5 @@
+let idade = 25
+
+console.log(typeof idade)
+console.log(typeof 15)
+console.log(typeof -13)
--- numbers/number1/script.js
+++
@@ -1,6 +0,0 @@
-let idade = 25
-
-console.log(typeof idade)
-
-console.log(typeof 13)
-console.log(typeof -12)
