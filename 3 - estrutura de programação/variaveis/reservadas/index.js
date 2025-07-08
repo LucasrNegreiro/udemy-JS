8bf00f5a9a779682be4de8de775d7a3c26eb4a12
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 14:58:05 -0300

    palavras reservadas

---
+++ 3 - estrutura de programação/variaveis/reservadas/index.js
@@ -0,0 +1,10 @@
+// Algumas palavras reservadas que não podem ser usadas em nome de variavel
+
+// let if = 'teste'
+// let break = 'nome'
+// let const = 'carro'
+// let switch  = 0
+
+// posso usar se eu alterar algo nela ex
+
+let switch2 = 'Lucas'
