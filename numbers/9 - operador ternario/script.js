2318da679275e947411c5d94768b685529c0055d
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 11:50:22 -0300

    operador ternario

---
+++ numbers/9 - operador ternario/script.js
@@ -0,0 +1,3 @@
+// operador ternario ? e : e um exemplo de if e else 3 e maior que 5?
+// se sim(?) me traga a resposta sim se não(:) me traga a outra resposta
+console.log( 3 > 5 ? 'sim' : '3 não e maior que 5')
