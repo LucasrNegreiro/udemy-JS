453625c581d9ff0e00ee8f1623691c6290d24043
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 12:05:20 -0300

    Update script.js

--- numbers/9 - operador ternario/script.js
+++ numbers/9 - operador ternario/script.js
@@ -1,3 +1,5 @@
 // operador ternario ? e : e um exemplo de if e else 3 e maior que 5?
 // se sim(?) me traga a resposta sim se não(:) me traga a outra resposta
 console.log(  3 > 5 && 5 != 3 ? 'sim' : '3 não e maior que 5')
+
+console.log (3 < 5 && 3 != 5 ? 'sim 3 é menor que 5 e diferente dele': 'não')
