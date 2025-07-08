6077b45c8bd0285e1ee0c7107ca00d6e9811f71c
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 11:19:12 -0300

    string avançado
    manipulando concatenação,  template literals e eventos especiais com o \

---
+++ numbers/5 - string avançado/script.js
@@ -0,0 +1,6 @@
+// \ permite adicionar eventos especiais na string como o \n que da uma quebra de linha
+console.log('Lucas \n Rocha \n negreiro')
+// template literal em cráse permite chamar uma variavel na string ou adicionar um comando
+console.log(`A soma de 2 + 2 é ${2+2}`)
+// concatenação de string
+console.log('salada'+' de '+'fruta')
