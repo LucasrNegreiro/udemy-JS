10a108ec2f346faa544e87e9e262fe3ad1a22513
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 11:47:34 -0300

    comparação e operador logico

---
+++ numbers/7 comparação/script.js
@@ -0,0 +1,14 @@
+//  > = maior que
+console.log( 10 > 2)
+//  < = menor que
+console.log( 10 < 2)
+// >= maior ou igual a
+console.log(10 >= 2)
+//  <= menor ou igual a
+console.log (20 <=2 )
+// == igual
+console.log( 5 == '5')
+// != diferente
+console.log(5 != 5)
+// === identico a
+console.log (5 === '5')
---
+++ numbers/8 - operadores logicos/script.js
@@ -0,0 +1,8 @@
+
+// aqui se os dois n forem verdadeiros ele trará falso pq os operador &&: ele quer os dois valores iguais
+console.log(3 > 2 && 2 == 3)
+console.log(3>2 && 3 != 2)
+// se um e verdadeiro e o outro falso ele tras verdadeiro pq o || se le como OU: ou um ou outro
+console.log (5 > 3 ||  3 === 1)
+// ele inverte a o valor se for verdade vira false e se for false vira true != : 2 não e 2?
+console.log(2 != 2)
