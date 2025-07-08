7ebf988dab6db9f3f960b8305c63542e85db1512
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 16:16:05 -0300

    Update index.js

--- 3 - estrutura de programação/math/index.js
+++ 3 - estrutura de programação/math/index.js
@@ -3,3 +3,10 @@
 let numeroMaior = Math.max(1, 15 ,2 ,32 ,56)
 
 console.log(numeroMaior)
+
+let menor = Math.min(1 ,15 ,28 ,32)
+
+console.log(menor)
+
+let arredondar = Math.round(1.8)
+console.log(arredondar)
