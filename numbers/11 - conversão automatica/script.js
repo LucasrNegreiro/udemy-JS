7a0c4542f198812fc69932347de6dbc114d17eb0
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 12:15:38 -0300

    conversão automatica do js

---
+++ numbers/11 - conversão automatica/script.js
@@ -0,0 +1,13 @@
+// quando o js converte automaticamente sem dar erro 
+
+// invez de somar ele converte a uma concatenação adicionado o 3 ao 2 ficando 23
+console.log ('2' + 3)
+
+// converte o null em 0 pois nenhum numero foi definido para multiplicar com 5
+console.log(5 * null)
+
+// ele subtrai automaticamente mesmo o 5 sendo uma string pois o - so tem a função de subtração
+console.log('5' - 3)
+
+// ele tras o NaN not a number pois ambas são strings escritas e não number
+console.log('dois' * 'três')
