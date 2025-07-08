c2e45923614ff229109d37b094a5475ca3d0ec85
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-08 14:52:37 -0300

    Convenção
    convenções de como declarar uma variavel

---
+++ 3 - estrutura de programação/variaveis/convenção/index.js
@@ -0,0 +1,11 @@
+let nome3= 'teste'
+let $nome= 'teste1'
+let _nome= 'teste2'
+
+console.log(`${nome3}, ${$nome}, ${_nome}`)
+
+let Nome = 'teste4'
+
+let meuPrimeiroNome = 'teste5'
+
+// varias formas de poder declarar uma variavel 
