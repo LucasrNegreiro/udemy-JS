6710a8e5db6725cfbe4e61b5ef53545aa7a52047
LucasrNegreiro <Lucasrn7@icloud.com>
2025-07-10 11:32:36 -0300

    identação
    identação e usada para melhorar a leitura do codigo deixando mais limpo

---
+++ 3 - estrutura de programação/identação/script.js
@@ -0,0 +1,10 @@
+let x = 0
+let y = 0
+
+if(x + y == 0){
+    for(i =0; i <= 10; i++){
+        console.log(y = i)
+        let res = x*i
+        console.log(`${x} vezes ${y} é igual a: ${res}`)
+    }
+}
